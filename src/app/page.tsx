8f7036310a5ed869abcca659e4ebfe7e83aa6183
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-08 21:39:04 +0300

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import BaseAbout from '@/components/sections/layouts/about/BaseAbout';
 import HowToBuy3D from '@/components/sections/layouts/howtobuy/3DHTB';
 import BigNumberTokenomics from '@/components/sections/layouts/tokenomics/BigNumberTokenomics';
 import BaseFooter from '@/components/sections/layouts/footer/BaseFooter';
+import { Globe } from '@/components/bento/contents/Globe';
 
 const theme: SiteTheme = {
   styleVariant: "funAndTrendy",
